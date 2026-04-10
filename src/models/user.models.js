0a5674d5d191fa38cdd5e470f8869bec02b7f668
sakshisingh0101 <sakshiaingh0101>
2026-04-11 01:49:55 +0530

    user model fixed

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -59,40 +59,5 @@ userSchema.methods.generateRefreshToken=function(){
         }
 )
 }
-userSchema.pre("save" , async function(next){
-    if(!this.isModified("password")) return next;
-    this.password= await bycrypt.hash(this.password , 10);
-    next;
-})
-userSchema.methods.isPassword = async function(password){
-return await bcrypt.compare(password , this.password)
-}
-userSchema.methods.generateAccessToken=function(){
-     return jwt.sign(
-        {  _id:this._id,
-           
-            email:this.email,
 
-
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-        }
-)
-}
-userSchema.methods.generateRefreshToken=function(){
-    return jwt.sign(
-        {  _id:this._id,
-            
-            email:this.email,
-
-
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-        }
-)
-}
 export const User = mongoose.model("User" , userSchema)
